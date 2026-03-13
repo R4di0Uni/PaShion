8f56f137becaf3fc2749bf3402b3a36c80a16eab
Daniel Almeida <115008409+R4di0Uni@users.noreply.github.com>
2026-03-13 17:02:06 +0000

    UpdateCanvas

--- Assets/Scripts/DrawingSystem.cs
+++ Assets/Scripts/DrawingSystem.cs
@@ -1,38 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawingSystem : MonoBehaviour
 {
     public RenderTexture drawingTexture;
     public Material drawMaterial;
 
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            Vector2 pos = Input.mousePosition;
+            Vector2 localPoint;
 
-            RenderTexture.active = drawingTexture;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                rectTransform,
+                Input.mousePosition,
+                null,
+                out localPoint))
+            {
+                Rect rect = rectTransform.rect;
 
-            GL.PushMatrix();
-            GL.LoadPixelMatrix(0, drawingTexture.width, drawingTexture.height, 0);
-
-            drawMaterial.SetPass(0);
-
-            GL.Begin(GL.QUADS);
-
-            float size = 20;
-
-            GL.Vertex3(pos.x - size, pos.y - size, 0);
-            GL.Vertex3(pos.x + size, pos.y - size, 0);
-            GL.Vertex3(pos.x + size, pos.y + size, 0);
-            GL.Vertex3(pos.x - size, pos.y + size, 0);
-
-            GL.End();
-            GL.PopMatrix();
-
-            RenderTexture.active = null;
+                if (rect.Contains(localPoint))
+                {
+                    float x = (localPoint.x - rect.x) / rect.width * drawingTexture.width;
+                    float y = drawingTexture.height - ((localPoint.y - rect.y) / rect.height * drawingTexture.height);
+                    Draw(x, y);
+                }
+            }
         }
+    }
+
+    void Draw(float x, float y)
+    {
+        RenderTexture.active = drawingTexture;
+
+        GL.PushMatrix();
+        GL.LoadPixelMatrix(0, drawingTexture.width, drawingTexture.height, 0);
+
+        drawMaterial.SetPass(0);
+
+        float size = 20;
+
+        GL.Begin(GL.QUADS);
+        GL.Vertex3(x - size, y - size, 0);
+        GL.Vertex3(x + size, y - size, 0);
+        GL.Vertex3(x + size, y + size, 0);
+        GL.Vertex3(x - size, y + size, 0);
+        GL.End();
+
+        GL.PopMatrix();
+
+        RenderTexture.active = null;
     }
 }
